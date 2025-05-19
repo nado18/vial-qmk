491bb845d806e02242e4e280d010296af8d6d2ff
Eric Nadeau <nado18@gmail.com>
2025-05-19 11:00:43 -0400

    Left half working!

--- keyboards/szrkbd/debug/keymaps/default/keymap.c
+++ keyboards/szrkbd/debug/keymaps/default/keymap.c
@@ -1,5 +1,7 @@
 #include QMK_KEYBOARD_H
 
+#ifdef RIGHT
+
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
     KC_Y,    KC_U,    KC_I,     KC_O,   KC_P,
@@ -8,6 +10,19 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_RALT, KC_RWIN, KC_RCTL
   )
 };
+
+#else
+
+const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT(
+    KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,
+    KC_A,    KC_S,    KC_D,     KC_F,    KC_G,
+    KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,
+                      KC_LCTL,  KC_LWIN, KC_LALT
+  )
+};
+
+#endif
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
