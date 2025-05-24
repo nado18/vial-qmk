37ba97821c5c7a7375f1cfa93197c399aeb8b26a
Eric Nadeau <nado18@gmail.com>
2025-05-24 15:47:49 -0400

    Deleting commented-out code

--- keyboards/szrkbd/debug/config.h
+++ keyboards/szrkbd/debug/config.h
@@ -4,11 +4,3 @@
 
 #define SERIAL_USART_SPEED 921600
 
-/*
-#define DIRECT_PINS { \
-  { B2, B10, C10, C11, B6 }, \
-  { B0, B1,  C12, D2,  B7 }, \
-  { C5, B12, B3,  B4,  B5 }, \
-  { C4, A1,  C13          } \
-}
-*/
